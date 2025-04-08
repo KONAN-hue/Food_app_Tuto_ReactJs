5d932a1f162abf06aee15a06c206c99f0b04b326
KONAN-hue <hilarion.konan2024@esatic.edu.ci>
2025-04-08 02:00:30 +0000

    ReactJS foodapp Tuto49

---
+++ src/components/Search.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "4964a8a84ecb46dcaf768fa1c5737a19";
+
+export default function Search() {
+  const [query, setQuery] = useState("pizza");
+  // Syntax of the useEffect hook
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+    }
+    fetchFood();
+  }, [query]);
+  return (
+    <div>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+}
